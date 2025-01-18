1fba4b19f621eede29a8a222a14154c83aad4d4d
Ayan <ayanroyal1174@gmail.com>
2025-01-18 22:24:15 +0530

    this is sixth

---
+++ sixth.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class sixth{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float p = sc.nextInt();
+        float r= sc.nextInt();
+        float t = sc.nextInt();
+        float si = (p*r*t)*100;
+        System.out.println(si );
+
+    }
+}
