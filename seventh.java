c37eaa00ceb02ed019ea311ac0d8fabc62ee89a8
Ayan <ayanroyal1174@gmail.com>
2025-01-18 22:29:22 +0530

    this is seventh

---
+++ seventh.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class seventh{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float p = sc.nextInt();
+        float r= sc.nextInt();
+
+        System.out.println(2*p*r);
+
+    }
+}
