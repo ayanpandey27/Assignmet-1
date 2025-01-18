e1c4d7fea7b448309203e58cb837c40b7b5890d4
Ayan <ayanroyal1174@gmail.com>
2025-01-18 22:17:29 +0530

    this is fifth

---
+++ fifth.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class fifth{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int h = sc.nextInt();
+        float pi = 3.14f;
+        float vol = pi *n*n*h;
+        System.out.println("volume is"+vol);
+
+    }
+}
