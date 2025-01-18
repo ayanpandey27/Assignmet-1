4b9b07b583042e00e0c575570a400e5d5abda886
Ayan <ayanroyal1174@gmail.com>
2025-01-18 22:38:22 +0530

    this is 8th

---
+++ eight.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class eight {
+
+    public static double calculatePower(double base, double exponent) {
+        return Math.pow(base, exponent);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the base: ");
+        double base = scanner.nextDouble();
+
+        System.out.print("Enter the exponent: ");
+        double exponent = scanner.nextDouble();
+
+        double result = calculatePower(base, exponent);
+        System.out.println(base + " raised to the power of " + exponent + " is " + result);
+
+        scanner.close();
+    }
+}
