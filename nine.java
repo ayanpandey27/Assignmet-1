e203e003d7c17d7eb81ad2215b0ba56ef8f52923
Ayan <ayanroyal1174@gmail.com>
2025-01-18 22:47:10 +0530

    nine code

---
+++ nine.java
@@ -0,0 +1,14 @@
+// Source code is decompiled from a .class file using FernFlower decompiler.
+import java.util.Scanner;
+
+class nine {
+
+   public static void main(String[] var0) {
+      Scanner var1 = new Scanner(System.in);
+      int var2 = var1.nextInt();
+      int var3 = var1.nextInt();
+      int var4 = var1.nextInt();
+      int var5 = (var2 + var3 + var4) / 3;
+      System.out.println(var5);
+   }
+}
