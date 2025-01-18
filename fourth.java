6cd6e0c8e4720c58771564fdd134e7fa3b404d6e
Ayan <ayanroyal1174@gmail.com>
2025-01-18 22:09:47 +0530

    this is fourth

---
+++ fourth.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class fourth{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        float pi = 3.14f;
+        float area = pi * n*n;
+        System.out.println("Area is "+area);
+
+
+    }
+}
